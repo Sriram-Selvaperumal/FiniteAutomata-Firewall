b3310a9fb3c6f541a61107e44dad113f675b451e
Sriram-Selvaperumal <sriramselvaperumal@icloud.com>
2025-08-04 19:53:16 +0530

    Packet Sniffer Updated

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,4 +1,5 @@
 from scapy.all import sniff, IP, TCP, Raw
+from automata import is_valid_http_request
 
 def packet_callback(packet):
     if IP in packet:
@@ -7,10 +8,13 @@ def packet_callback(packet):
         proto = 'TCP' if TCP in packet else 'Other'
         
         if Raw in packet:
-            payload = str(packet[Raw].load)
+            payload = packet[Raw].load.decode(errors="ignore")
             if "HTTP" in payload or "GET" in payload or "POST" in payload:
-                print(f"[{proto}] {ip_src} --> {ip_dst}")
+                is_valid = is_valid_http_request(payload)
+                status = "ALLOWED" if is_valid else "BLOCKED"
+
+                print(f"[{proto}] {ip_src} --> {ip_dst} | Status: {status}")
                 print(f"Payload: {payload}\n")
 
-print("Protocol-Aware Sniffer Running...")
+print("Finite Automata Firewall Running...")
 sniff(prn=packet_callback, store=False)
